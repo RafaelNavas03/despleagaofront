6b86bb6ec159930a829f31fa8b0aa94f1719f4c4
bryan robalino <eduardochuez_2003@outlook.com>
2024-02-04 14:16:45 -0500

    arreglado el boton de paypal

--- src/Clientes/Paypal.jsx
+++ src/Clientes/Paypal.jsx
@@ -2,50 +2,57 @@
 
 import React, { useContext,useRef, useEffect } from 'react';
 import { CartContext } from '../context/CarritoContext';
+import { notification } from 'antd';
 
-const PayPal = () => {
+
+
+const PayPal = ({ onSuccess }) => {
   const [cart, setCart] = useContext(CartContext);
   const totalPrice = cart.reduce((acc, curr) => acc + curr.quantity * curr.price, 0);
 
-
+ 
   useEffect(() => {
-    console.log('PayPal Component - useEffect 1');
-    if (window.paypal) {
-      window.paypal.Buttons({
-        createOrder: function (data, actions) {
-          // Set up the transaction details, including the total amount
-          return actions.order.create({
-            purchase_units: [
-              {
-                amount: {
-                  value: totalPrice.toFixed(2), // Ensure the total amount is formatted correctly
+    const script = document.createElement('script');
+    script.src = 'https://www.paypal.com/sdk/js?client-id=AY_g64Csg6RjTCN5__-9jz9nMDeHdyhtkKOFIs_05dz6xfmBHuEGM2Vb54n81yZJkXTeEIGROUue1XEW';
+    script.async = true;
+    document.body.appendChild(script);
+    script.onload = () => {
+      if (window.paypal) {
+        window.paypal.Buttons({
+          createOrder: function (data, actions) {
+            return actions.order.create({
+              purchase_units: [
+                {
+                  amount: {
+                    value: totalPrice.toFixed(2),
+                  },
                 },
-              },
-            ],
-          });
-        },
-        onApprove: function (data, actions) {
-          // Capture the funds from the transaction
-          return actions.order.capture().then(function (details) {
-            // Call a function to handle the successful payment
-            handlePaymentSuccess(details);
-          });
-        },
-        onError: function (err) {
-          // Handle errors during the transaction
-          console.error('Error during PayPal transaction:', err);
-        },
-      }).render('#paypal-button-container'); // Specify the ID of the container where the button will be rendered
+              ],
+            });
+          },
+          onApprove: function (data, actions) {
+            return actions.order.capture().then(function (details) {
+              handlePaymentSuccess(details);
+            });
+          },
+          onError: function (err) {
+            console.error('Error during PayPal transaction:', err);
+          },
+        }).render('#paypal-button-container');
+      }
     }
   }, [totalPrice]);
 
   const handlePaymentSuccess = (details) => {
-    // Handle the successful payment, e.g., update the order status, clear the cart, etc.
     console.log('Payment completed successfully:', details);
-
-    // Clear the cart after a successful payment
     setCart([]);
-    CerrarModal();
+    onSuccess(); 
+
+    notification.success({
+      message: 'Pago Exitoso',
+      description: '¡El pago se ha completado con éxito!',
+    });
+
   };
 
   return (
