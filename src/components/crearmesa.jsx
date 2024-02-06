1a0480dc52fa779d6f3ed1b2c88abaf9da278057
lreyesp26 <lreyesp@uteq.edu.ec>
2024-02-05 20:10:58 -0500

    Registro falta

--- src/components/crearmesa.jsx
+++ src/components/crearmesa.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Form, Input, Button, Select, message } from 'antd';
+import React from "react";
+import { Form, Input, Button, Select, message, InputNumber } from "antd";
 
 const { Option } = Select;
 
@@ -13,17 +13,17 @@ const CrearMesa = () => {
         formData.append(key, values[key]);
       });
 
-      const response = await fetch('http://127.0.0.1:8000/Mesas/crear/', {
-        method: 'POST',
+      const response = await fetch("http://127.0.0.1:8000/Mesas/crear/", {
+        method: "POST",
         body: formData,
       });
 
       if (response.ok) {
         const data = await response.json();
-        console.log('Respuesta del servidor:', data.message); 
+        console.log("Respuesta del servidor:", data.message);
 
         // Mostrar mensaje de éxito
-        message.success('Mesa creada con éxito');
+        message.success("Mesa creada con éxito");
 
         // Limpiar el formulario
         form.resetFields();
@@ -31,19 +31,31 @@ const CrearMesa = () => {
         throw new Error(`Error en la solicitud: ${response.statusText}`);
       }
     } catch (error) {
-      console.error('Error al enviar la solicitud:', error.message);
+      console.error("Error al enviar la solicitud:", error.message);
 
       // Mostrar mensaje de error
-      message.error('Error al crear la mesa. Por favor, inténtalo de nuevo.');
+      message.error("Error al crear la mesa. Por favor, inténtalo de nuevo.");
     }
   };
 
   return (
-    <Form form={form} onFinish={onFinish} labelCol={{ span: 6 }} wrapperCol={{ span: 16 }}>
-      <Form.Item name="observacion" label="Observación" rules={[
-        { required: true, message: 'Por favor, ingresa la observación' },
-        { max: 500, message: 'La observación no puede tener más de 500 caracteres' },
-      ]}>
+    <Form
+      form={form}
+      onFinish={onFinish}
+      labelCol={{ span: 6 }}
+      wrapperCol={{ span: 16 }}
+    >
+      <Form.Item
+        name="observacion"
+        label="Observación"
+        rules={[
+          { required: true, message: "Por favor, ingresa la observación" },
+          {
+            max: 500,
+            message: "La observación no puede tener más de 500 caracteres",
+          },
+        ]}
+      >
         <Input.TextArea />
       </Form.Item>
       <Form.Item name="estado" label="Estado" rules={[{ required: true }]}>
@@ -60,8 +72,17 @@ const CrearMesa = () => {
           <Option value="0">Desactivada</Option>
         </Select>
       </Form.Item>
-      <Form.Item name="max_personas" label="Máximo de Personas" rules={[{ required: true }]}>
-        <Input />
+      <Form.Item
+        name="max_personas"
+        label="Máximo de Personas"
+        rules={[
+          {
+            required: true,
+            message: "Por favor, ingresa el número máximo de personas",
+          },
+        ]}
+      >
+        <InputNumber min={0} />
       </Form.Item>
       <Form.Item wrapperCol={{ offset: 6, span: 16 }}>
         <Button type="primary" htmlType="submit">
